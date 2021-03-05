b7113c267ccf9e2e7758ab64f0f5617dc86eeae2
bvenceslas <bvenceslas@gmail.com>
2021-03-05 15:22:24 +0200

    create express server

---
+++ app.js
@@ -0,0 +1,8 @@
+const express = require('express');
+
+const app = express();
+app.use(express.json());
+
+
+const port = process.env.PORT || 4000;
+app.listen(port, () => console.log(`PicaRest listening on ${port}`));
