69e13debab4f6d6a5dba7a93c1713f136a62684d
bvenceslas <bvenceslas@gmail.com>
2021-03-07 19:56:26 +0200

    add persons

--- routes/api.js
+++ routes/api.js
@@ -1,17 +1,22 @@
 const router = require('express').Router();
 const Person = require('../model/person.model');
+const { upload } = require('../helper/helper');
 
 // api test
 router.route('/test').get(async (req, res) => {
-    res.json('PicaRest API works perfectly ...');
+    res.json('PicaRest API works perfectly ...' + upload);
 });
 
 // save a person
-router.post('/persons', async (req, res) => {
+router.post('/persons', upload.single('photo'), async (req, res) => {
     const { firstName, lastName, photo } = req.body;
 
     try {        
-        const newPerson = new Person(req.body);
+        const newPerson = new Person({
+            firstName: firstName,
+            lastName: lastName,
+            photo: req.file.path
+        });
         const savePerson = newPerson.save();
         console.log(savePerson);
         return res.json({msg: 'Saved successfuly'});
