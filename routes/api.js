bc7f7d1e12a4a8005ca25837b28ed96fe32681ae
bvenceslas <bvenceslas@gmail.com>
2021-03-07 20:05:10 +0200

    get a particular person

--- routes/api.js
+++ routes/api.js
@@ -28,12 +28,23 @@ router.post('/persons', upload.single('photo'), async (req, res) => {
 // get a person
 router.get('/persons', async (req, res) => {
     try {        
-        const person = await Person.find();
+        const persons = await Person.find();
+        return res.send(persons);
+    } catch (error) {
+        return res.send({errorMessage: error})
+    }
+});
+
+// get a particular person
+router.get('/persons/:id', async (req, res) => {
+    try {        
+        const person = await Person.findById(req.params.id);
         return res.send(person);
     } catch (error) {
         return res.send({errorMessage: error})
     }
-})
+});
+
 
 module.exports = router;
 
